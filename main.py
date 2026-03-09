02eb089e48e252a92caaba6b7f5247b8b3b225a1
Nikita-Lartsyn <lartsynnikita@gmail.com>
2026-03-09 22:12:34 +0100

    add delete user feature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def add_user():
-    name = input("Enter user name: ")
+    name = input("Enter user name: ").strip()
+
+    if not name:
+        print("Name cannot be empty")
+        return
 
     with open("users.txt", "a") as f:
         f.write(name + "\n")
@@ -11,14 +15,39 @@ def show_users():
             for i, line in enumerate(f, start=1):
                 print(f"{i}. {line.strip()}")
     except FileNotFoundError:
-        print("No users yet.")
+        print("\nNo users yet.")
+
+
+def delete_user():
+    with open("users.txt", "r") as f:
+        users = f.readlines()
+
+        for i, user in enumerate(users, start=1):
+            print(f"{i}. {user.strip()}")
+
+    try:
+        index = int(input("\nWho do you want to delete? ")) - 1
+    except ValueError:
+        print("\nThis isn't a number")
+        return
+
+    if 0 <= index < len(users):
+        del users[index]
+    else:
+        print("\nInvalid user number")
+        return
+
+    with open("users.txt", "w") as f:
+        f.writelines(users)
+    print("\nUser deleted")
 
 
 def menu():
     while True:
         print("\n1. Add user")
         print("2. Show users")
-        print("3. Exit")
+        print("3. Delete user")
+        print("4. Exit")
 
         choice = input("Choose an option: ")
 
@@ -27,6 +56,8 @@ def menu():
         elif choice == "2":
             show_users()
         elif choice == "3":
+            delete_user()
+        elif choice == "4":
             break
         else:
             print("Invalid option")
